d5fc8eb2f25a9bc958bde21f5b53d65d1636898a
Minh Pham <mpham@naver.com>
2019-03-05 20:09:13 +0100

    Add default search term

--- src/components/app.js
+++ src/components/app.js
@@ -10,6 +10,10 @@ class App extends React.Component {
     selectedVideo: null
   };
 
+  componentDidMount() {
+    this.onTermSubmit("quiero volver");
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: {
