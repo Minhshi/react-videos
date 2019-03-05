ae641f99eed2d9dddbd27bc4453139de9faf5da8
Minh Pham <mpham@naver.com>
2019-03-05 17:57:51 +0100

    Add state for video results

--- src/components/app.js
+++ src/components/app.js
@@ -3,18 +3,26 @@ import youtube from "../api/youtube";
 import SearchBar from "./search_bar";
 
 class App extends React.Component {
+  state = {
+    videos: []
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
+    this.setState({
+      videos: response.data.items
+    })
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        {this.state.videos.length} videos
       </div>
     );
   }
