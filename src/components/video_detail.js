9da0794c8526504cbb4d639b1376d4e4cee6b531
Minh Pham <mpham@naver.com>
2019-03-05 19:29:38 +0100

    Add style to VideoDetail component

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -2,7 +2,15 @@ import React from "react";
 
 class VideoDetail extends React.Component {
   render() {
-    return <div>Video Detail</div>;
+    if (!this.props.video) {
+      return "Loading...";
+    }
+    return (
+      <div className="ui segment">
+        <h4 className="ui header">{this.props.video.snippet.title}</h4>
+        <p>{this.props.video.snippet.description}</p>
+      </div>
+    );
   }
 }
 
