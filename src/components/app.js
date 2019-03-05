331a6c0a5ffb3fc157b4cf464770901a95ced19f
Minh Pham <mpham@naver.com>
2019-03-05 17:44:59 +0100

    Add async and await to API function

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,8 @@ import youtube from "../api/youtube";
 import SearchBar from "./search_bar";
 
 class App extends React.Component {
-  onTermSubmit = term => {
-    youtube.get("/search", {
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
       params: {
         q: term
       }
